dcfcec2deff876be8eb4efa3a6bf507269c60f67
Josh Langford <eaglesform@gmail.com>
2014-09-19 07:43:37 -0600

    moved files

--- README.md
+++
@@ -1,1 +0,0 @@
-This is a hodgepodge of code I have found useful & wanted to retain publicly for all to use.
--- match_by_percentage.rb
+++
@@ -1,6 +0,0 @@
-# take string into var
-# gsub non-numerics
-# split on spaces
-# compare number & size of words, pad with asterisks
-# compare letter-to-letter
-# produce a match percentage
